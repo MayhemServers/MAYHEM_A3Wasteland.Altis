8d637ad3ff7cf1bced8d713ec0a1e8d32ef7e0dd
osuapoc <springer.83@gmail.com>
2015-05-14 20:55:59 -0400

    Allow players to choose whether environmental is on or off.  Using Creampie's button and script.

--- client/systems/playerMenu/dialog/player_settings.hpp
+++ client/systems/playerMenu/dialog/player_settings.hpp
@@ -183,8 +183,17 @@ class playerSettings {
 			x = 0.02; y = 0.57;
 			w = 0.125; h = 0.033 * safezoneH;
 		};
-
+		
+		class btnDistanceEffects : w_RscButton {
+			idc = -1;
+			text = "Effects";
+			onButtonClick = "[] execVM 'addons\disableEnvironment\disableEnvironment.sqf'";
+			x = 0.158; y = 0.5;
+			w = 0.125; h = 0.033 * safezoneH;
+		};
+		
 		class btnDistanceInsane : w_RscButton {
+			idc = -1;
 			text = "Insane";
 			onButtonClick = "setViewDistance 5000;";
 			x = 0.158; y = 0.57;
